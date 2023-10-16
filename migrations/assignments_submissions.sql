adfbc0a15887863509fe29fa1ad30f81df61918a
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-10-16 15:40:17 -0700

    Queries added, bugFixed regarding isertion of data into the tables.

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -2,12 +2,12 @@ CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
+  duration INTEGER,
   day INTEGER,
-  chapter INTEGER,
-  duration INTEGER
+  chapter INTEGER
 );
 
-CREATE TABLE assignments_submissions (
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id  INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
